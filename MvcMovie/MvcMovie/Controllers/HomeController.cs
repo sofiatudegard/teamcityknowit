3666c70cf4627a7bf25154097be88f43a57acdb8
sofiatudegard <sofia@tudegard.se>
2014-02-28 09:35:08 +0100

    Fejkar ett fel för att se hur den reagerar i teamcity
    Signed-off-by: sofiatudegard <sofia@tudegard.se>

--- MvcMovie/MvcMovie/Controllers/HomeController.cs
+++ MvcMovie/MvcMovie/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace MvcMovie.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
+        {[{
             return View();
         }
 
